a4ff5c6d05dfc284a33450694e2289c7a41d8ad7
lipseyj <lipseyj@g.cofc.edu>
2020-11-05 08:52:52 -0500

    git commit -m "<message>"

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#create different parts of html template for easy formatting
+html='<!DOCTYPE html><html><meta charset="utf-8"><head><style>body{background-color: darkred;}h3{color: green; background-color: black; text-decoration: underline;}h4{color: lightgreen}p{color: yellow;}</style><title>Test Cases</title></head><body>'
+
 #iterate through each testCase file
 for testCase in ../testCases/*
 do
@@ -47,17 +50,23 @@ read -r output<$outputFile
 #output=$(head -n 1 outputFile)
 #output=$out
 
-echo "Test Num: $testNum
-$req
-Method tested: $meth
-Inputs: $inputs
-Expected Output: $oracle
-Actual Output: $output
-" >> ../reports/report.txt
+#adds formatted version of the test output to the html file
+currentTest="<h3>Test Num: $testNum</h3><h4>$req</h4><p><br>Method tested: $meth<br>Inputs: $inputs<br>Expected Output: $oracle<br> Actual Output: $output<br></p>"
+html="$html$currentTest<br>"
+
+#previous code for creating a text file
+#echo "Test Num: $testNum
+#$req
+#Method tested: $meth
+#Inputs: $inputs
+#Expected Output: $oracle
+#Actual Output: $output
+#" >> ../reports/report.txt
 
 done
+#adds closing html tag and stores it all in report.html
+echo "$html</body></html>" > report.html
 
-cat ../reports/report.txt
-
-
-	
+xdg-open report.html
+cat ../reports/report.html
+#cat ../reports/report.txt
