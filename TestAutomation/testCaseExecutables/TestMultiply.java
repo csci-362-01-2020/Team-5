9918f864773942275f91a9c53ec043e4b41ecf38
lipseyj <lipseyj@g.cofc.edu>
2020-11-23 15:39:44 -0500

    added TestMultiply with fault

--- TestAutomation/testCaseExecutables/TestMultiply.java
+++ TestAutomation/testCaseExecutables/TestMultiply.java
@@ -20,9 +20,17 @@ public class TestMultiply {
 		OptionalDouble oracle = new OptionalDouble(Double.valueOf(args[2]));
 		
 		OptionalDouble oD = new OptionalDouble(valueOfDouble);
+		
+		//*Original, CORRECT code
 		OptionalDouble oDToMultiply = new OptionalDouble(valueToMultiply);
+		/* Code with fault injected (should cause tests 16, 17, 19, and 20 to fail)
+		OptionalDouble oDToMultiply = new OptionalDouble((-1)* valueToMultiply); */
+		//faulty code ends
 		
 		OptionalDouble product = oD.multiply(oDToMultiply);
+		//added if statement so that -0.0 is reset to 0.0
+		if(product.equals(new OptionalDouble(-0.0)))
+			product = new OptionalDouble(0.0);
 		double prodValue = product.getValue();
 		String pass = (oracle.equals(product)) ? "Passed" : "Failed";
 		
