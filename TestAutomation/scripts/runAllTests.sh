fda92d05ac95357d22887a0665610411f38409dd
lipseyj <lipseyj@g.cofc.edu>
2020-11-22 19:26:37 -0500

    updated html so each test case has its own html report with corresponding number

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 
 #create different parts of html template for easy formatting
-htmlOld="<!DOCTYPE html>
-<html><meta charset="utf-8"><head>
-<style>body{background-color: darkred; color: lightgreen} th{color: black; text-decoration: underline;} table, th, td{text-align: center; vertical-align: middle; width: 50%; border-collapse: collapse; border: 2px solid black;}</style>
-<title>Test Cases</title></head><body><table><h3><tr><th>Test Num</th><th>Req</th><th>Method Tested</th><th>Inputs</th><th>Expected Outputs</th><th>Actual Output</th></h3></tr>"
+#htmlOld="<!DOCTYPE html><html><meta charset="utf-8"><head><style>body{background-color: darkred; color: lightgreen} th{color: black; text-decoration: underline;} table, th, td{text-align: center; vertical-align: middle; width: 50%; border-collapse: collapse; border: 2px solid black;}</style><title>Test Cases</title></head><body><table><h3><tr><th>Test Num</th><th>Req</th><th>Method Tested</th><th>Inputs</th><th>Expected Outputs</th><th>Actual Output</th></h3></tr>"
 
+#html="<!DOCTYPE html><html><meta charset="utf-8"><head><style>body{background-color: lightBlue; color: black; font-family: Courier, monospace;} th{color: black; text-decoration: underline;} table, th, td{text-align: center; vertical-align: middle; width: 50%; border-collapse: collapse; border: 2px solid black; padding: .5em} table{margin: auto;}</style><title>Test Cases</title></head><body><table><h3><tr><th>Test Num</th><th>Req</th><th>Method Tested</th><th>Inputs</th><th>Expected Outputs</th><th>Actual Output</th><th>Test Result</th></h3></tr>"
+
+#main html
 html="<!DOCTYPE html>
-<html><meta charset="utf-8"><head>
-<style>body{background-color: aliceBlue; color: black; font-family: Courier, monospace;} th{color: black; text-decoration: underline;} table, th, td{text-align: center; vertical-align: middle; width: 50%; border-collapse: collapse; border: 2px solid black; padding: .5em} table{margin: auto;}</style>
-<title>Test Cases</title></head><body><table><h3><tr><th>Test Num</th><th>Req</th><th>Method Tested</th><th>Inputs</th><th>Expected Outputs</th><th>Actual Output</th><th>Test Result</th></h3></tr>"
+<html><meta charset="utf-8"><style>body{background-color: white; color: black; font-family: Courier, monospace;}</style><title>Test Cases</title><nav><ul>"
 
-# compile dependencies
+
+#each test case is put in the same temporary string html and transfered
+htmlT="<!DOCTYPE html><html><meta charset="utf-8"><head>"
+
+count=1
+
 cd ..
 cd ./testCaseExecutables
+for testCase in ../testCases/*
+do
+html=$html"<li><a href="../reports/report$count.html">Test Case: $count</a></li>"
+count=$((count+1))
+done
+
+#makes html files for number of test cases
+
+html=$html"</ul></nav><head>"
+
+
+# compile dependencies
+#cd ..
+#cd ./testCaseExecutables
 #javac -Djava.ext.dirs=./testCaseExecutables
 #javac -d "testCaseExecutables" #FloatingPointFormatter.java OptionalDouble.java IgnoreCaseStringComparator.java
-
+count=1
 
 #iterate through each testCase file
 for testCase in ../testCases/*
@@ -60,15 +77,20 @@ output=${outputArray[0]}
 pass=${outputArray[1]}
 
 
+##adds formatted version of the test output to the html file
+#html="$html<tr><td>$testNum</td><td>$req</td><td>$meth</td><td>$inputs</td><td>$oracle</td><td>$output</td><td>$pass</td></tr>"
+##"$html</table></body></html>" > ../reports/report"$count".html
+#html=$htmlTemp
+
 #adds formatted version of the test output to the html file
-html="$html<tr><td>$testNum</td><td>$req</td><td>$meth</td><td>$inputs</td><td>$oracle</td><td>$output</td><td>$pass</td></tr>"
+htmlT="$htmlT<title>Test Case"$count"</title><style>body{background-color: lightBlue; color: black; font-family: Courier, monospace;} th{color: black; text-decoration: underline;} table, th, td{text-align: center; vertical-align: middle; width: 50%; border-collapse: collapse; border: 2px solid black; padding: .5em} table{margin: auto;}</style><title>Test Cases</title></head><body><table><h3><tr><th>Test Num</th><th>Req</th><th>Method Tested</th><th>Inputs</th><th>Expected Outputs</th><th>Actual Output</th><th>Test Result</th></h3></tr><tr><td>$testNum</td><td>$req</td><td>$meth</td><td>$inputs</td><td>$oracle</td><td>$output</td><td>$pass</td></tr>"
+echo "$htmlT</table></body></html>" > ../reports/report"$count".html
 
+htmlT="<!DOCTYPE html><html><meta charset="utf-8"><head>"
 
-
-
+count=$((count+1))
 done
 #adds closing html tag and stores it all in report.html
 echo "$html</table></body></html>" > ../reports/report.html
 
 xdg-open ../reports/report.html 
-
